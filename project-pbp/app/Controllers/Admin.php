5eb0fe4aba9986f703ca676ca3936910dc8d5a7d
RafliFerdian25 <ferdianrafli25@gmail.com>
2021-10-23 12:23:07 +0700

    cek

--- project-pbp/app/Controllers/Admin.php
+++ project-pbp/app/Controllers/Admin.php
@@ -51,18 +51,16 @@ class Admin extends BaseController
 
     public function transaksi()
     {
-        $query = $this->penjualanModel->getTransaksi();
-        // $builder->where('name', $name);
-        $transaksi = $query->where('name', '5');
-        
-        // $data = [
-        //     'title' => 'Data Transaksi | Sumber Jaya Furniture',
-        //     'transaksi' => $transaksi
-        // ];
+        $db = \Config\Database::connect();
+        $query = $db->query("SELECT penjualan.idpenjualan AS 'idpenjualan', penjualan.tgl_penjualan AS 'tgl_penjualan', barang.nama AS 'nama_barang', pelanggan.nama AS 'nama_pembeli', penjualan.total_item AS jumlah, penjualan.total_harga AS 'harga_total', status.nama AS 'status' FROM (((((penjualan JOIN detail_penjualan ON penjualan.idpenjualan = detail_penjualan.idpenjualan) JOIN barang ON detail_penjualan.idbarang = barang.idbarang) JOIN pelanggan ON penjualan.idpelanggan = pelanggan.idpelanggan) JOIN status_penjualan ON penjualan.idpenjualan = status_penjualan.idpenjualan) JOIN status ON status_penjualan.idstatus = status.idstatus) ORDER BY penjualan.idpenjualan;");
+        $transaksi = $query->getResultArray();
 
-        dd($transaksi);
+        $data = [
+            'title' => 'Data Transaksi | Sumber Jaya Furniture',
+            'transaksi' => $transaksi
+        ];
 
-        // return view('admin/data_transaksi', $data);
+        return view('admin/data_transaksi', $data);
     }
 
     public function detail()
